9a1292e697c8082f8a705e9cce3651ef276d2eb5
unknown <jpwingfi@.Gentiva.GHSNet.Com>
2011-06-20 13:39:10 -0400

    Adding uniqueness support to shams

---
+++ Machinist.Net/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Machinist.Net.Extensions
+{
+    static class ObjectExtensions
+    {
+        internal static void ThrowIfNull(this object value)
+        {
+            if (value == null)
+                throw new ArgumentNullException();
+        }
+    }
+}
